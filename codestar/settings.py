dc89946d5b69d1bfcccd67d763af97bc07989495
Anders Holst <andersthereseholst@gmail.com>
2024-02-03 22:00:58 +0000

    added post models

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-andersh82-ittib-340sm22qcy6.ws-eu108.gitpod.io', '.herokuapp.com']
 
@@ -91,6 +91,11 @@ DATABASES = {
 dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeanyapp.com",
+    "https://*.herokuapp.com",
+    "https://*.8000-andersh82-ittib-340sm22qcy6.ws-eu108.gitpod.io"
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
