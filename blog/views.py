db4c1f53919e52f0526df6aac21b1df2928c8650
Anders Holst <andersthereseholst@gmail.com>
2024-02-05 20:28:10 +0000

    added post_detail view

--- blog/views.py
+++ blog/views.py
@@ -11,7 +11,7 @@ class PostList(generic.ListView):
     paginate_by = 6
 
 
-    def post_detail(request, slug):
+def post_detail(request, slug):
     """
     Display an individual :model:`blog.Post`.
 
@@ -24,12 +24,6 @@ class PostList(generic.ListView):
 
     :template:`blog/post_detail.html`
     """
-
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
-
-    return render(
-        request,
-        "blog/post_detail.html",
-        {"post": post},
-    )
+    return render(request, "blog/post_detail.html", {"post": post},)
